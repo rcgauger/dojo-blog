10c3a4afc36773ff98a351e05c2a9c6b1add3cf1
Ray Gauger <rcgauger@gmail.com>
2024-05-14 17:05:19 -0400

    cleaned up for json db file

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,9 +1,9 @@
-const BlogList = ({ blogs, title }) => {
+const BlogList = ({ blogs }) => {
 
     return ( 
-      <div className='blog-list'>
-            <h2>{ title }</h2>
-            {blogs.map((blog) => (
+      <div className="blog-list">
+
+            {blogs.map(blog => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{ blog.title }</h2>
                     <p>Written by { blog.author }</p>
--- src/Home.js
+++ src/Home.js
@@ -1,25 +1,24 @@
-import { useState , useEffect } from 'react';
-import BlogList from './BlogList';
+import { useEffect, useState } from "react";
+import BlogList from "./BlogList";
 
 const Home = () => {
-    const [blogs, setBlogs] = useState(null);
+  const [blogs, setBlogs] = useState(null)
 
-    useEffect(() => {
-        fetch('http://localhost:8000/blogs')
-            .then(res => {
-                return res.json()
-            })
-            .then((data) => {
-                console.log(data);
-                setBlogs(data);
-            });
-    }, [])
+  useEffect(() => {
+    fetch('http://localhost:8000/blogs')
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        setBlogs(data);
+      })
+  }, [])
 
-    return(
-        <div className="home">
-           {blogs && <BlogList blogs={blogs} title="All Blogs!"></BlogList>  }  
-        </div>   
-      );
+  return (
+    <div className="home">
+      {blogs && <BlogList blogs={blogs} />}
+    </div>
+  );
 }
  
 export default Home;
